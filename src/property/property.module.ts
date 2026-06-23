eacd8cdc04fa205f936adaf650ae7efd03238e40
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-06-23 11:29:13 +0200

    feat: module of the controller property

--- src/property/property.module.ts
+++ src/property/property.module.ts
@@ -1,4 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { PropertyController } from './property.controller';
+import { APP_PIPE } from '@nestjs/core';
 
-@Module({})
+@Module({
+  controllers: [PropertyController],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    },
+  ],
+})
 export class PropertyModule {}
